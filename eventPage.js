cbd4a93a9601886491fdd2cdf66057cb42679f7c
Ryan Chen <ryan@Ryans-MacBook-Pro.local>
2015-04-12 14:14:35 -0700

    Add log and save to file feature
    Signed-off-by: Ryan Chen <ryan@Ryans-MacBook-Pro.local>

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,13 @@
+// When sending a message from a content script,
+// there is no ready-made method to deliver a message
+// to the correct DevTools page instance associated
+// with the current tab. As a workaround, you can
+// have the DevTools page establish a long-lived
+// connection with the background page, and have the
+// background page keep a map of tab IDs to
+// connections, so it can route each message to
+// the correct connection.
+
 var connections = {};
 
 chrome.runtime.onConnect.addListener(function(port) {
@@ -30,7 +40,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (tabId in connections) {
       connections[tabId].postMessage(request);
     } else {
-      console.warn("Tab not found in connection list.");
+      console.warn("Tab not found in connection list. tabId is ", tabId);
     }
   } else {
     console.warn("sender.tab not defined.");
--- panel/panel.js
+++ panel/panel.js
@@ -1,15 +1,48 @@
+
+// Build connection to eventPage
 var eventPageConnection = chrome.runtime.connect({
   name: "panel"
-});
+}),
+  // The log for all recorded actions
+  actionLog = [];
 
+// Init or register the current panel in eventPage
 eventPageConnection.postMessage({
   name: 'init',
   tabId: chrome.devtools.inspectedWindow.tabId
 });
 
-$(function() {
-  eventPageConnection.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      $('.path').text(request.path);
-    });
+eventPageConnection.onMessage.addListener(function (request, sender, sendResponse) {
+
+  var newRow = $('<tr></tr>')
+  .append('<td class="action-name">' + request.action + '</td>')
+  .append('<td class="action-path">' + request.path + '</td')
+  .append('<td class="action-mouse-pos">' + JSON.stringify(request.currentMousePos) + '</td>')
+  .append('<td class="action-key-code">' + request.keyCode + '</td>');
+
+  // Append the new request message into console
+  $('table.action-table tbody')
+  .prepend(newRow);
+
+  // Put the message into actionLog
+  actionLog.push(request);
+});
+
+// Save to file button
+$('.save-btn').click(function () {
+
+  // Create a blob file and down load it
+  // From http://bgrins.github.io/devtools-snippets/#console-save
+  var blob = new Blob([JSON.stringify(actionLog)], {
+  	type: 'text/json'
+  }),
+  fileUrl = URL.createObjectURL(blob),
+  a = document.createElement('a'),
+  e = document.createEvent('MouseEvents');
+
+  a.download = 'action-log.json';
+  a.href = fileUrl;
+  a.dataset.downloadUrl = ['text/json', a.download, a.href].join(':');
+  e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  a.dispatchEvent(e);
 });
